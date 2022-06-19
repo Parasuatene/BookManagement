2ad05872d1a02b0005f5059f0c4b740214ee124e
Parasuatene <andoooo9.y.1127@gmail.com>
2022-06-19 23:31:26 +0900

    [fix] ハッシュ値の算出メソッド

--- src/myapi/HashGenerator.java
+++ src/myapi/HashGenerator.java
@@ -21,7 +21,7 @@ public class HashGenerator {
 		try {
 			// SHA3-256（SHA-3）
 			MessageDigest sha3_256 = MessageDigest.getInstance("SHA3-256");
-			byte[] sha3_256_result = sha3_256.digest("aa".getBytes());
+			byte[] sha3_256_result = sha3_256.digest(text.getBytes());
 			return String.format("%040x", new BigInteger(1, sha3_256_result));
 		} catch (NoSuchAlgorithmException e) {
 			throw new RuntimeException(e);
