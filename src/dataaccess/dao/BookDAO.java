2ae11e2c75124c8f98da536e9b173a19e147c793
Parasuatene <andoooo9.y.1127@gmail.com>
2022-06-19 23:33:03 +0900

    [update] select時に貸出情報も取得するように変更

--- src/dataaccess/dao/BookDAO.java
+++ src/dataaccess/dao/BookDAO.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import dataaccess.value.Book;
+import dataaccess.value.RentalControl;
 
 public class BookDAO extends BaseDAO {
 
@@ -27,9 +28,20 @@ public class BookDAO extends BaseDAO {
 		PreparedStatement preparedStatement = null;
 		ResultSet resultSet = null;
 		try {
+//			// SQLの定義
+//			String sql = "SELECT tb.id, tb.title, tb.author, tb.publisher, tb.img_path, tb.discription"
+//						 + ", vr.id, vr.user_id, vr.book_id, vr.start_date, vr.schedule_date, vr.end_date "
+//						 + "FROM t_book AS tb "
+//						 + "LEFT OUTER JOIN v_latest_rental_info AS vr "
+//						 + "ON tb.id = vr.book_id";
 			// SQLの定義
-			String sql = "SELECT id, title, author, publisher, img_path, discription "
-						 + "FROM t_book";
+			String sql = "SELECT tb.id AS tb_id, tb.title AS tb_title, tb.author AS tb_author"
+						 + ", tb.publisher AS tb_publisher, tb.img_path AS tb_img_path, tb.discription AS tb_discription"
+						 + ", vr.id AS vr_id, vr.user_id AS vr_user_id, vr.book_id AS vr_book_id"
+						 + ", vr.start_date AS vr_start_date, vr.schedule_date AS vr_schedule_date, vr.end_date AS vr_end_date "
+						 + "FROM t_book AS tb "
+						 + "LEFT OUTER JOIN v_latest_rental_info AS vr "
+						 + "ON tb.id = vr.book_id";
 			// SQLの作成
 			preparedStatement = getConnection().prepareStatement(sql);
 			// SQLの実行
@@ -38,12 +50,22 @@ public class BookDAO extends BaseDAO {
 			List<Book> bookList = new ArrayList<Book>();
 			while(resultSet.next()) {
 				Book book = new Book();
-				book.setId(resultSet.getInt("id"));
-				book.setTitle(resultSet.getString("title"));
-				book.setAuthor(resultSet.getString("author"));
-				book.setPublisher(resultSet.getString("publisher"));
-				book.setImgPath(resultSet.getString("img_path"));
-				book.setDiscription(resultSet.getString("discription"));
+				book.setId(resultSet.getInt("tb_id"));
+				book.setTitle(resultSet.getString("tb_title"));
+				book.setAuthor(resultSet.getString("tb_author"));
+				book.setPublisher(resultSet.getString("tb_publisher"));
+				book.setImgPath(resultSet.getString("tb_img_path"));
+				book.setDiscription(resultSet.getString("tb_discription"));
+
+				RentalControl rentalControl = new RentalControl();
+				rentalControl.setId(resultSet.getInt("vr_id"));  // TODO: 貸出管理番号が取得できるようにする
+				rentalControl.setUserId(resultSet.getString("vr_user_id"));
+				rentalControl.setBookId(resultSet.getInt("vr_book_id"));
+				rentalControl.setStartDate(resultSet.getDate("vr_start_date"));
+				rentalControl.setScheduleDate(resultSet.getDate("vr_schedule_date"));
+				rentalControl.setEndDate(resultSet.getDate("vr_end_date"));
+				book.setRentalControl(rentalControl);
+
 				bookList.add(book);
 			}
 			return bookList;
